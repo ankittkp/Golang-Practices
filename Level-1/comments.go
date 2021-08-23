3e5bcdc2d95892a712f5e7682f9180c77c63f3dc
Ankit Kumar <ankit.2official@gmail.com>
2021-08-23 16:34:15 +0530

    Add files via upload

---
+++ Level-1/comments.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("adding single line comment")
+	//comment here
+	fmt.Println("adding multiple line comments")
+	/*
+	hey there
+	how are you?
+	where have you been?
+	 */
+}

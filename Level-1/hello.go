3275d44851f51d216e1ba2dc4b42ad959fb89619
Ankit Kumar <ankit.2official@gmail.com>
2021-08-23 16:12:19 +0530

    Add files via upload

---
+++ Level-1/hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Display Your Name and Address here: ")
+	fmt.Println("for run this file, change directory : cd dir/path")
+	fmt.Println("Enter go run command to run the file: go run hello.go")
+}
